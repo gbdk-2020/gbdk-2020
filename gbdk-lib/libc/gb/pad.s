78a7a0fb34a2a4b85961185e4b8e0ec02dc9d10d
toxa <untoxa@mail.ru>
2020-07-06 16:12:55 +0300

    Optimized joypad routines

--- gbdk-lib/libc/gb/pad.s
+++ gbdk-lib/libc/gb/pad.s
@@ -7,17 +7,16 @@
 .padup::
 _waitpadup::
 	PUSH	AF		; Save modified registers
-	PUSH	BC
 1$:
-	LD	B,#0xFF
+	LD	D,#0xFF
 2$:
 	CALL	.jpad
 	OR	A		; Have all buttons been released?
 	JR	NZ,1$		; Not yet
 
-	DEC	B
+	DEC	D
 	JR	NZ,2$
-	POP	BC		; Restore registers
+	
 	POP	AF
 	RET
 
@@ -27,17 +26,15 @@ _waitpadup::
 	;;   0x40 - Select  0x04 - Up
 	;;   0x20 - B	    0x02 - Left
 	;;   0x10 - A	    0x01 - Right
+_joypad::
 .jpad::
-	PUSH	BC		; Save modified registers
 	LD	A,#0x20
 	LDH	(.P1),A		; Turn on P15
 
 	LDH	A,(.P1)		; Delay
 	LDH	A,(.P1)
-	CPL
 	AND	#0x0F
-	SWAP	A
-	LD	B,A
+	LD	E,A
 	LD	A,#0x10
 	LDH	(.P1),A		; Turn on P14
 	LDH	A,(.P1)		; Delay
@@ -46,35 +43,25 @@ _waitpadup::
 	LDH	A,(.P1)
 	LDH	A,(.P1)
 	LDH	A,(.P1)
-	CPL
 	AND	#0x0F
-	OR	B
 	SWAP	A
-	LD	B,A
+	OR	E
+	CPL
+	LD	E,A
 	LD	A,#0x30
 	LDH	(.P1),A		; Turn off P14 and P15 (reset joypad)
-	LD	A,B
-	POP	BC		; Restore registers
+	LD	A,E
 	RET
 
-	;; Wait for the key in B to be pressed
+	;; Wait for the key to be pressed
+_waitpad::
+	LDA	HL,2(SP)	; Skip return address
+	LD	D,(HL)
+	;; Wait for the key in D to be pressed
 .wait_pad::
 1$:
 	CALL	.jpad		; Read pad
-	AND	B		; Compare with mask?
+	AND	D		; Compare with mask?
 	JR	Z,1$		; Loop if no intersection
 	RET
 
-_joypad::
-	CALL	.jpad
-	LD	E,A		; Return result in DE
-	RET
-
-_waitpad::
-	PUSH	BC
-	LDA	HL,4(SP)	; Skip return address and registers
-	LD	B,(HL)
-	CALL	.wait_pad
-	LD	E,A		; Return result in DE
-	POP	BC
-	RET
