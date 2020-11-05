05a81b7d4cdc70ad8adda5e2bbc8c800e7d0cdc5
toxa <untoxa@mail.ru>
2020-11-05 18:08:38 +0300

    hiramcpy() minor optimizations

--- gbdk-lib/libc/gb/hiramcpy.s
+++ gbdk-lib/libc/gb/hiramcpy.s
@@ -3,35 +3,33 @@
 	;; BANKED:	checked
 	.area	_BASE
 
+_hiramcpy::
+	LDA	HL,4(SP)	; Skip return address and registers
+	LD	E,(HL)		; E = dst
+	LDA	HL,7(SP)
+	LD	A,(HL-)
+	LD	D, A		; D = n
+	LD	A,(HL-)		; HL = src
+	LD	L,(HL)
+	LD	H,A
+
 	;; Copy memory zone to HIRAM
 	;; 
 	;; Entry conditions
-	;;   C = destination
-	;;   B = length
+	;;   E = destination
+	;;   D = length
 	;;   HL = source
 	;; 
-	;; Register used: AF, BC, HL
+	;; Preserves: BC
 .hiramcpy::
+	LD	A,E
+	LD	E,C
+	LD	C,A
 1$:
 	LD	A,(HL+)
 	LDH	(C),A
 	INC	C
-	DEC	B
+	DEC	D
 	JR	NZ,1$
-	RET
-
-_hiramcpy::
-	PUSH	BC
-
-	LDA	HL,4(SP)	; Skip return address and registers
-	LD	C,(HL)		; C = dst
-	LDA	HL,7(SP)
-	LD	A,(HL-)		; B = n
-	LD	B, A
-	LD	A,(HL-)		; HL = src
-	LD	L,(HL)
-	LD	H,A
-	CALL	.hiramcpy
-
-	POP	BC
+	LD	C,E
 	RET
