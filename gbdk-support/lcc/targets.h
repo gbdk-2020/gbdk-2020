71527e25cb0f8efdc0f3d250b142652fb6fb719d
bbbbbr <bbbbbr@users.noreply.github.com>
2021-08-09 00:40:56 -0700

    lcc: split ports/platforms data into separate targets.c/.h - Fixlist / llist[0] defaults are now attached to classes per port/plat - Adds llist defaults for z80 sms/gg platform

---
+++ gbdk-support/lcc/targets.h
@@ -0,0 +1,38 @@
+// targets.h
+
+#include <stdbool.h>
+
+#ifndef _LCC_TARGETS_H
+#define _LCC_TARGETS_H
+
+// For Linker list defaults (llist0)
+typedef struct arg_entry {
+    char searchkey[255];
+    char addflag[255];
+    char addvalue[255];
+    bool found;
+} arg_entry;
+
+// For Port/Platform specific settings
+typedef struct CLASS {
+    const char *port;
+    const char *plat;
+    const char *default_plat;
+    const char *rom_extension;
+    const char *cpp;
+    const char *include;
+    const char *com;
+    const char *as;
+    const char *bankpack;
+    const char *ld;
+    const char *ihxcheck;
+    const char *mkbin;
+    arg_entry  *llist0_defaults;
+    int         llist0_defaults_len;
+} CLASS;
+
+
+extern CLASS classes[];
+extern int classes_count;
+
+#endif // _TARGETS_H
