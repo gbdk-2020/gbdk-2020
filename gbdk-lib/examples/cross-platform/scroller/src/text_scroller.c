ce3c57764996907a754e3989b156e921b83b5783
Toxa <untoxa@mail.ru>
2021-08-27 16:41:22 +0300

    examples: cross-platform scroller example

---
+++ gbdk-lib/examples/cross-platform/scroller/src/text_scroller.c
@@ -0,0 +1,52 @@
+#include <gbdk/platform.h>
+#include <stdint.h>
+#include <stdio.h>
+
+const uint8_t scanline_offsets_tbl[] = {0, 1, 2, 3, 3, 2, 1, 0, 0, 1, 2, 3, 3, 2, 1, 0};
+const uint8_t * scanline_offsets = scanline_offsets_tbl;
+
+#define SCROLL_POS 15
+
+uint8_t scroller_x = 0;
+void scanline_isr() {
+}
+
+const uint8_t scroller_text[] = "This is a text scroller demo for GBDK-2020. You can use ideas, that are "\
+"shown in this demo, to make different parallax effects, scrolling of tilemaps which are larger than 32x32 "\
+"tiles and TEXT SCROLLERS, of course! Need to write something else to make this text longer than 256 characters. "\
+"The quick red fox jumps over the lazy brown dog. 0123456789.          ";
+
+const uint8_t * scroller_next_char = scroller_text;
+uint8_t * scroller_vram_addr;
+uint8_t * base, * limit;
+
+void main() {
+    printf("Scrolling %d chars", sizeof(scroller_text) - 1);
+    
+    HIDE_LEFT_COLUMN;    
+    base = (uint8_t *)((uint16_t)get_bkg_xy_addr(0, SCROLL_POS) & (DEVICE_SCREEN_MAP_ENTRY_SIZE==1?0xffe0:0xffc0));
+    limit = base + (DEVICE_SCREEN_BUFFER_WIDTH * DEVICE_SCREEN_MAP_ENTRY_SIZE);
+
+    scroller_vram_addr = base + ((DEVICE_SCREEN_X_OFFSET + DEVICE_SCREEN_WIDTH) * DEVICE_SCREEN_MAP_ENTRY_SIZE);
+    if (scroller_vram_addr >= limit) scroller_vram_addr = base;
+    
+    set_vram_byte(scroller_vram_addr, *scroller_next_char - 0x20);
+    
+    while (1) {
+        scroller_x++;
+        move_bkg(scroller_x, 0);
+        if ((scroller_x & 0x07) == 0) {
+            // next letter
+            scroller_next_char++;
+            if (*scroller_next_char == 0) scroller_next_char = scroller_text;
+            
+            // next vram position
+            scroller_vram_addr += DEVICE_SCREEN_MAP_ENTRY_SIZE;
+            if (scroller_vram_addr >= limit) scroller_vram_addr = base;
+            
+            // put next char
+            set_vram_byte(scroller_vram_addr, *scroller_next_char - 0x20);
+        }
+        wait_vbl_done();        
+    }
+}
