36f34ae66dd437bbf01377ae2441bb99c605ce98
Toxa <untoxa@mail.ru>
2021-12-09 23:14:19 +0300

    rollback conditional definition of bool

--- gbdk-lib/include/stdbool.h
+++ gbdk-lib/include/stdbool.h
@@ -29,19 +29,11 @@
 #ifndef __SDC51_STDBOOL_H
 #define __SDC51_STDBOOL_H 1
 
-#ifdef __SDCC
 /* Define true and false of type _Bool in a way compatible with the preprocessor (see N 2229 for details). */
 #define true ((_Bool)+1)
 #define false ((_Bool)+0)
 
 #define bool _Bool
-#else
-#define true 1
-#define false 0
-
-#define bool unsigned char
-#endif
-
 #define __bool_true_false_are_defined 1
 
 #endif
