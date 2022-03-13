3ad0db7cd202def3e84e62d9e5b1f492dffba628
Toxa <untoxa@mail.ru>
2022-03-13 23:46:18 +0300

    longer wait between the SGB packet transfers

--- gbdk-lib/libc/targets/gbz80/duck/global.s
+++ gbdk-lib/libc/targets/gbz80/duck/global.s
@@ -388,6 +388,33 @@
         OAMB_PAL1       = 4 ; Palette number; 0,1 (DMG)
         OAMB_BANK1      = 3 ; Bank number; 0,1 (GBC)
 
+        ;; SGB packets
+        .PAL_01         = 0x00
+        .PAL_23         = 0x01
+        .PAL_03         = 0x02
+        .PAL_12         = 0x03
+        .ATTR_BLK       = 0x04
+        .ATTR_LIN       = 0x05
+        .ATTR_DIV       = 0x06
+        .ATTR_CHR       = 0x07
+        .SOUND          = 0x08
+        .SOU_TRN        = 0x09
+        .PAL_SET        = 0x0A
+        .PAL_TRN        = 0x0B
+        .ATRC_EN        = 0x0C
+        .TEST_EN        = 0x0D
+        .ICON_EN        = 0x0E
+        .DATA_SND       = 0x0F
+        .DATA_TRN       = 0x10
+        .MLT_REQ        = 0x11
+        .JUMP           = 0x12
+        .CHR_TRN        = 0x13
+        .PCT_TRN        = 0x14
+        .ATTR_TRN       = 0x15
+        .ATTR_SET       = 0x16
+        .MASK_EN        = 0x17
+        .OBJ_TRN        = 0x18
+
         ;; CPU detection
         .DMG_TYPE       = 0x01 ; Original GB or Super GB
         .MGB_TYPE       = 0xFF ; Pocket GB or Super GB 2
