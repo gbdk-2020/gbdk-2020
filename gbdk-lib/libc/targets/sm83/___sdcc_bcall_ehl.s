9f32f7c9aa1f2f224962eb35738447d9b5a2c523
Toxa <untoxa@mail.ru>
2022-05-28 23:18:53 +0300

    minor style

--- gbdk-lib/libc/targets/sm83/___sdcc_bcall_ehl.s
+++ gbdk-lib/libc/targets/sm83/___sdcc_bcall_ehl.s
@@ -3,16 +3,16 @@
 	.area _HOME
 
 ___sdcc_bcall_ehl::			; Performs a long call.
-	ldh	a,(__current_bank)
+	ldh	a, (__current_bank)
 	push	af			; Push the current bank onto the stack
 	ld	a, e
-	ldh	(__current_bank),a
-	ld	(.MBC_ROM_PAGE),a	; Perform the switch
+	ldh	(__current_bank), a
+	ld	(.MBC_ROM_PAGE), a	; Perform the switch
 	rst	0x20
 	pop	bc			; Pop the old bank
-	ld c, a
-	ld a, b
-	ldh	(__current_bank),a
-	ld	(.MBC_ROM_PAGE),a
-	ld a, c
+	ld	c, a
+	ld	a, b
+	ldh	(__current_bank), a
+	ld	(.MBC_ROM_PAGE), a
+	ld	a, c
 	ret
