b858ae8d5cced22f8ac37ff48fa9fc6c3860b98f
MichelIwaniec <michel.iwaniec@gmail.com>
2022-06-08 19:30:20 +0100

    Add iNES output support to makebin * Add new input parameter -N to output ines * Add new structure nes_opt_s for describing iNES flags * Add function write_ines_header to write 16-byte iNES header to file from nes_opt_s * Set default iNES values to mapper2, 32kB PRG, horizontal mirroring * Modify read_ihx to allow applying an optional rom_base_offset to addresses, set to 0x8000 for iNES

--- gbdk-support/makebin/makebin.c
+++ gbdk-support/makebin/makebin.c
@@ -93,6 +93,7 @@ usage (void)
            "  -s romsize     size of the binary file (default: rom banks * 16384)\n"
            "  -Z             generate GameBoy format binary file\n"
            "  -S             generate Sega Master System format binary file\n"
+           "  -N             generate Famicom/NES format binary file\n"
            "  -o bytes       skip amount of bytes in binary file\n"
 
            "SMS format options (applicable only with -S option):\n"
@@ -149,6 +150,16 @@ struct sms_opt_s
   BYTE rom_size;                  /* Doesn't have to be the real size, needed for checksum */
   BYTE region_code;               /* Region code Japan/Export/International and SMS/GG */
   BYTE version;                   /* Game version */
+};
+
+struct nes_opt_s
+{
+    BYTE mapper;
+    BYTE num_prg_banks;
+    BYTE num_chr_banks;
+    bool vertical_mirroring;
+    bool four_screen;
+    bool battery;
 };
 
 void
@@ -592,7 +603,7 @@ noi2sym (char *filename)
 }
 
 int
-read_ihx (FILE *fin, BYTE **rom, int *size, int *real_size, struct gb_opt_s *o)
+read_ihx (FILE *fin, BYTE **rom, int *size, int *real_size, struct gb_opt_s *o, int rom_base_offset)
 {
   int record_type;
 
@@ -639,6 +650,8 @@ read_ihx (FILE *fin, BYTE **rom, int *size, int *real_size, struct gb_opt_s *o)
         }
       // add linear address extension
       addr |= extaddr;
+      // Subtract ROM base offset
+      addr -= rom_base_offset;
       // TODO: warn for unreachable banks according to chosen MBC
       if (record_type > 1)
         {
@@ -646,7 +659,7 @@ read_ihx (FILE *fin, BYTE **rom, int *size, int *real_size, struct gb_opt_s *o)
           return 0;
         }
 
-      if (addr + nbytes > *size)
+      if (addr + nbytes > *size || (addr < 0 && nbytes > 0))
         {
           // If auto-size is enabled, grow rom bank size by power of 2 when needed
           if (o->rom_banks_autosize)
@@ -686,6 +699,30 @@ read_ihx (FILE *fin, BYTE **rom, int *size, int *real_size, struct gb_opt_s *o)
   return 1;
 }
 
+void write_ines_header(FILE* fout, struct nes_opt_s* nes_opt)
+{
+  char id_string[] = { 0x4E, 0x45, 0x53, 0x1A };  
+  BYTE flags6 = ((nes_opt->mapper & 0xF) << 4) | 
+                 (nes_opt->four_screen << 3) |
+                 (nes_opt->battery << 1) |
+                 nes_opt->vertical_mirroring;
+  BYTE flags7 = (nes_opt->mapper & 0xF0);
+  BYTE flags8 = 0;
+  BYTE flags9 = 0;
+  BYTE flags10 = 0;
+  BYTE padding[5] = { 0, 0, 0, 0, 0, };
+  // "NES" + end-of-file
+  fwrite(&id_string, sizeof(char), 4, fout);
+  fwrite(&nes_opt->num_prg_banks, sizeof(BYTE), 1, fout);
+  fwrite(&nes_opt->num_chr_banks, sizeof(BYTE), 1, fout);
+  fwrite(&flags6, sizeof(BYTE), 1, fout);
+  fwrite(&flags7, sizeof(BYTE), 1, fout);
+  fwrite(&flags8, sizeof(BYTE), 1, fout);
+  fwrite(&flags9, sizeof(BYTE), 1, fout);
+  fwrite(&flags10, sizeof(BYTE), 1, fout);
+  fwrite(padding, sizeof(BYTE), 5, fout);
+}
+
 int
 main (int argc, char **argv)
 {
@@ -697,6 +734,7 @@ main (int argc, char **argv)
   int ret;
   int gb = 0;
   int sms = 0;
+  int nes = 0;
 
   struct gb_opt_s gb_opt = {.cart_name="",
                             .licensee_str={'0', '0'},
@@ -716,6 +754,14 @@ main (int argc, char **argv)
   struct sms_opt_s sms_opt = {.rom_size=0xc,
                               .region_code=4,
                               .version=0 };
+
+  struct nes_opt_s nes_opt = {
+                              .mapper = 2,
+                              .num_prg_banks = 2,
+                              .num_chr_banks = 0,
+                              .vertical_mirroring = 0,
+                              .four_screen = 0,
+                              .battery = 0 };
 
 #if defined(_WIN32)
   setmode (fileno (stdout), O_BINARY);
@@ -890,6 +936,11 @@ main (int argc, char **argv)
           sms = 1;
           break;
 
+        case 'N':
+          /* generate iNES binary file */
+          nes = 1;
+          break;
+
         case 'x':
 
           switch (argv[0][2])
@@ -997,7 +1048,7 @@ main (int argc, char **argv)
         }
     }
 
-  ret = read_ihx (fin, &rom, &size, &real_size, &gb_opt);
+  ret = read_ihx (fin, &rom, &size, &real_size, &gb_opt, nes ? 0x8000 : 0x0000);
 
   fclose (fin);
 
@@ -1027,6 +1078,8 @@ main (int argc, char **argv)
           memset (rom + size - offset, FILL_BYTE, offset);
         }
 
+      if (nes)
+        write_ines_header(fout, &nes_opt);
       fwrite (rom, 1, (pack ? real_size : size) - offset, fout);
 
       fclose (fout);
