f80d41ac6f07766e2829d2fab5386c4cae70f1cb
bbbbbr <bbbbbr@users.noreply.github.com>
2023-01-28 19:02:04 -0800

    png2asset: tidy up image utils a bit - header guard - move namespace out of header - harmonize on uint8_t - rename unpack function (Thx Michel)

--- gbdk-support/png2asset/image_utils.h
+++ gbdk-support/png2asset/image_utils.h
@@ -1,7 +1,10 @@
 // image_utils.h
 
+#ifndef _IMAGE_UTILS_H
+#define _IMAGE_UTILS_H
+
 #include <vector>
 
-using namespace std;
-
 bool image_indexed_ensure_8bpp(vector <unsigned char> &data, int width, int height, int bitdepth, int colortype);
+
+#endif
