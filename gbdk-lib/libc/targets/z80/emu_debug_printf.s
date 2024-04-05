b5e9f5e1c0279ea412a9c418dfdc9fbf1ea795d2
Toxa <untoxa@mail.ru>
2024-04-05 16:14:28 +0300

    refactor EMU_printf() - remove dependency from stdio.h (currently working in the Emulicious emu), add similar EMU_fmtbuf()

--- gbdk-lib/libc/targets/z80/emu_debug_printf.s
+++ gbdk-lib/libc/targets/z80/emu_debug_printf.s
@@ -3,42 +3,30 @@
         .title  "EMU_debug"
         .module EMU_debug
 
-        .globl  _sprintf
-
-        .area   _DATA
-
-ret_save:
-        .ds     0x02
-printf_buffer:
-        .ds     0x80
-
         .area   _HOME
 
-        ;; EMU_printf(fmt, ...)
+;;void EMU_printf(const unsigned char * fmt, ...)
 _EMU_printf::
-        di
-        pop de
-        ld hl, #ret_save
-        ld (hl), e
+        ld hl, #2
+        add hl, sp
+        ld e, (hl)
         inc hl
-        ld (hl), d
+        ld d, (hl)
+        inc hl
+        ex de, hl
 
-        ld de, #printf_buffer
-        push de
-        call _sprintf
-        pop hl
-
-        ld d,d
+        ld  d, d
         jr 1$
         .dw 0x6464
-        .dw 0x0001
-        .dw #printf_buffer
-        .dw 0
+        .dw 0x0200
 1$:
-        ld hl, #ret_save
-        ld a, (hl)
-        inc hl
-        ld h, (hl)
-        ld l, a
-        ei
-        jp (hl)
+        ret
+
+;;void EMU_fmtbuf(const unsigned char * fmt, void * data)
+_EMU_fmtbuf::
+        ld  d, d
+        jr 1$
+        .dw 0x6464
+        .dw 0x0200
+1$:
+        ret
