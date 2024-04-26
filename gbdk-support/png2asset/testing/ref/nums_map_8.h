c4a2b9a102a5e14b701d31691b07dc3f185e3965
bbbbbr <bbbbbr@users.noreply.github.com>
2024-04-26 03:28:29 -0700

    png2asset: test: update reference with (non-struct) -entity_tileset support

--- gbdk-support/png2asset/testing/ref/nums_map_8.h
+++ gbdk-support/png2asset/testing/ref/nums_map_8.h
@@ -11,7 +11,7 @@
 #define nums_map_8_TILE_H 8
 #define nums_map_8_WIDTH 80
 #define nums_map_8_HEIGHT 40
-#define nums_map_8_TILE_COUNT 30
+#define nums_map_8_TILE_COUNT 20
 #define nums_map_8_PALETTE_COUNT 1
 #define nums_map_8_COLORS_PER_PALETTE 4
 #define nums_map_8_TOTAL_COLORS 4
@@ -19,7 +19,7 @@
 
 BANKREF_EXTERN(nums_map_8)
 
-extern const uint8_t nums_map_8_tiles[480];
+extern const uint8_t nums_map_8_tiles[320];
 
 extern const unsigned char nums_map_8_map[50];
 #define nums_map_8_map_attributes nums_map_8_map
