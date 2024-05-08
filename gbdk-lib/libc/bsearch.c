2449eab9ce1d8515cb9d44534f2981802fdf19c8
Toxa <untoxa@mail.ru>
2024-05-08 17:07:01 +0300

    use REENTRANT wrapper macro instead of __reentrant SDCC attribute

--- gbdk-lib/libc/bsearch.c
+++ gbdk-lib/libc/bsearch.c
@@ -28,7 +28,7 @@
 
 #include <stdlib.h>
 
-void *bsearch(const void *key, const void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *) __reentrant)
+void *bsearch(const void *key, const void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *) REENTRANT)
 {
 	for(const char *left = base; nmemb;)
 	{
