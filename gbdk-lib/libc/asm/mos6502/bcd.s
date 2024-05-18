45fbc287a5d92a92fcc752bbf87b285642c29f30
Michel Iwaniec <michel.iwaniec@codegeneration.co.uk>
2024-05-18 12:43:57 +0100

    * Update license to GPLv2-LE

--- gbdk-lib/libc/asm/mos6502/bcd.s
+++ gbdk-lib/libc/asm/mos6502/bcd.s
@@ -1,27 +1,28 @@
 ;--------------------------------------------------------------------------
 ;  bcd.s
 ;
-;  MIT License
+;  Copyright (C) 2024, Michel Iwaniec
 ;
-;  Copyright (c) 2024 Michel Iwaniec
+;  This library is free software; you can redistribute it and/or modify it
+;  under the terms of the GNU General Public License as published by the
+;  Free Software Foundation; either version 2, or (at your option) any
+;  later version.
 ;
-;  Permission is hereby granted, free of charge, to any person obtaining a copy
-;  of this software and associated documentation files (the "Software"), to deal
-;  in the Software without restriction, including without limitation the rights
-;  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-;  copies of the Software, and to permit persons to whom the Software is
-;  furnished to do so, subject to the following conditions:
+;  This library is distributed in the hope that it will be useful,
+;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;  GNU General Public License for more details.
 ;
-;  The above copyright notice and this permission notice shall be included in all
-;  copies or substantial portions of the Software.
+;  You should have received a copy of the GNU General Public License 
+;  along with this library; see the file COPYING. If not, write to the
+;  Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
+;   MA 02110-1301, USA.
 ;
-;  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-;  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-;  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-;  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-;  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-;  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-;  SOFTWARE.
+;  As a special exception, if you link this library with other files,
+;  to produce an executable, this library does not by itself cause the 
+;  resulting executable to be covered by the GNU General Public License.
+;  This exception does not however invalidate any other reasons why the
+;  executable file might be covered by the GNU General Public License.
 ;--------------------------------------------------------------------------
 
 .module bcd
