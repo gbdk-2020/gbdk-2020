6d9291502d66218adc6069c9333702cb38cb846e
bbbbbr <bbbbbr@users.noreply.github.com>
2024-11-17 02:24:11 -0800

    LCC: NES target: turn on missing makebin arguments (mkbinlist)

--- gbdk-support/lcc/targets.c
+++ gbdk-support/lcc/targets.c
@@ -165,7 +165,7 @@ CLASS classes[] = {
       .bankpack     = "%bankpack% -plat=nes -mapper=30 $1 $2",
       .ld           = "%ld_6808% -a nes -n -i -j $1 %libs_include% $3 %crt0dir% $2",
       .ihxcheck     = "%ihxcheck% $2 $1",
-      .mkbin        = "%mkbin% -N -yo A $2 $3",
+      .mkbin        = "%mkbin% -N -yo A $1 $2 $3",
       .postproc     = "",
       .llist0_defaults    = llist0_defaults_nes,
       .llist0_defaults_len= ARRAY_LEN(llist0_defaults_nes),
