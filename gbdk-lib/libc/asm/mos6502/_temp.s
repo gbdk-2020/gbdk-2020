b1e1dc04a24ea1e8f981c6c8b220d2372bff850e
Michel Iwaniec <46843052+michel-iwaniec@users.noreply.github.com>
2025-05-05 23:53:09 +0100

    NES: Merge all temp registers into one file, to ensure a continuous section of ZP (#773)
    - Merge _dptr.s and _ret*.s into _temp.s
    
    - Add ALL_REGTEMPS_BEGIN / ALL_REGTEMPS_END labels so interrupts can easily push / pop temps

--- gbdk-lib/libc/asm/mos6502/_temp.s
+++ gbdk-lib/libc/asm/mos6502/_temp.s
@@ -1,5 +1,27 @@
 ; REGTEMP stack for temporaries used by SDCC
 ; Keep this in sync with NUM_TEMP_REGS in mos6502/gen.c
-
+;
+; Note: This has been patched by GBDK, to include variables in _ret01.s, _ret23.s, _ret4567.s
+; This is to ensure a continuous section of temporaries to simplify interrupt handling.
+;
 	.area	_ZP (PAG)
+ALL_REGTEMPS_BEGIN::
+DPTR::  .ds     2
 REGTEMP::	.ds	8
+___SDCC_m6502_ret0::
+        .ds 1
+___SDCC_m6502_ret1::
+        .ds 1
+___SDCC_m6502_ret2::
+        .ds 1
+___SDCC_m6502_ret3::
+        .ds 1
+___SDCC_m6502_ret4::
+        .ds 1
+___SDCC_m6502_ret5::
+        .ds 1
+___SDCC_m6502_ret6::
+        .ds 1
+___SDCC_m6502_ret7::
+        .ds 1
+ALL_REGTEMPS_END::
