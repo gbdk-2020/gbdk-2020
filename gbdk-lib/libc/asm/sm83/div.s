2f79d8943028fcfdc0be73b1f13e28403d711431
Toxa <untoxa@mail.ru>
2026-02-08 14:53:38 +0300

    changed attributing of div.s and mul.s for the SM83 target, because that is now the original implementation

--- gbdk-lib/libc/asm/sm83/div.s
+++ gbdk-lib/libc/asm/sm83/div.s
@@ -1,35 +1,10 @@
 ;--------------------------------------------------------------------------
 ;  div.s
 ;
-;  Copyright (C) 2000, Michael Hope
-;  Copyright (C) 2021, Sebastian 'basxto' Riedel (sdcc@basxto.de)
-;  Copyright (c) 2021, Philipp Klaus Krause
+;  Copyright (c) 2026, Phidias618
 ;
-;  This library is free software; you can redistribute it and/or modify it
-;  under the terms of the GNU General Public License as published by the
-;  Free Software Foundation; either version 2, or (at your option) any
-;  later version.
-;
-;  This library is distributed in the hope that it will be useful,
-;  but WITHOUT ANY WARRANTY; without even the implied warranty of
-;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-;  GNU General Public License for more details.
-;
-;  You should have received a copy of the GNU General Public License 
-;  along with this library; see the file COPYING. If not, write to the
-;  Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
-;   MA 02110-1301, USA.
-;
-;  As a special exception, if you link this library with other files,
-;  some of which are compiled with SDCC, to produce an executable,
-;  this library does not by itself cause the resulting executable to
-;  be covered by the GNU General Public License. This exception does
-;  not however invalidate any other reasons why the executable file
-;   might be covered by the GNU General Public License.
 ;--------------------------------------------------------------------------
 
-        ;; Originally from GBDK by Pascal Felber.
-	;; Updated by Phidias618
         .module divmod
         .area   _CODE
 
