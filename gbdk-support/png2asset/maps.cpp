5b66854425bb164d604d6791fee773954b6fb4bf
Toxa <untoxa@mail.ru>
2026-02-21 16:56:38 +0300

    remove the last (?) usage of the corner pixel peeking, use tile.pal instead

--- gbdk-support/png2asset/maps.cpp
+++ gbdk-support/png2asset/maps.cpp
@@ -122,7 +122,7 @@ void GetMap(PNG2AssetData* assetData)
 
             if(assetData->args->use_map_attributes)
             {
-                unsigned char pal_idx = assetData->image.data[y * assetData->image.w + x] >> assetData->args->bpp; //We can pick the palette from the first pixel of this tile
+                unsigned char pal_idx = tile.pal;
                 if(assetData->args->pack_mode == Tile::SGB)
                 {
                     props = props << 1; //Mirror flags in SGB are on bit 7
