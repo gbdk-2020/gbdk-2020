7ae47f6e65052cfd4c4da26c343ce7972375752b
Phidias618 <126189685+Phidias618@users.noreply.github.com>
2026-04-13 11:30:20 +0200

    Optimization of rand() for the sm83 and z80 targets

--- gbdk-lib/libc/asm/z80/rand.s
+++ gbdk-lib/libc/asm/z80/rand.s
@@ -11,38 +11,22 @@ ___rand_seed::
 _rand::	
 _randw::
 
-        LD HL, (.randval)
-        EX DE, HL
-        LD L, E
-        LD A, D
+        ld hl, (.randval)
+        LD d, h
+        ld e, l
 
-        SLA L                   ; * 16
-        RLA
-        SLA L
-        RLA
-        SLA L
-        RLA
-        SLA L
-        RLA
-        LD H, A                 ; Save randhi*16
+        add hl, hl
+        add hl, hl
+        add hl, hl
+        add hl, hl
+        add hl, de
+        ld de, #0x5C93
+        add hl, de
+        ld (.randval), hl
 
-        LD A, E                 ; Old randlo
-        ADD A, L                ; Add randlo*16
-        LD L, A                 ; Save randlo*17
-
-        LD A, H                 ; randhi*16
-        ADC A, D                ; Add old randhi
-        LD H, A                 ; Save randhi*17
-
-        LD A, L                 ; randlo*17
-        ADD A, #0x93
-        LD L, A
-        LD A, H                 ; randhi*17
-        ADC A, #0x5c
-        LD H, A
-        LD (.randval), HL
-        LD H, L
-        LD L, A
+        ld a, h
+        ld h, l
+        ld l, a
 
         RET
 
