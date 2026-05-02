1eeea4d288ec4af4b4d8b1d7da3a27e37efe1dca
Phidias618 <126189685+Phidias618@users.noreply.github.com>
2026-05-02 15:16:11 +0200

    Update delay.s

--- gbdk-lib/libc/targets/sm83/delay.s
+++ gbdk-lib/libc/targets/sm83/delay.s
@@ -8,61 +8,29 @@
 	;;   DE = number of milliseconds to delay (1 to 65536, 0 = 65536)
 	;; 
 	;; Register used: AF, DE
-	.CPMS	= 4194/4	; 4.194304 MHz
-
+	;; One millisecond last for 1048.576 M-cycles
+	;; This has been rounded to 1048.500 M-cycles
+loop:
+	; each iteration of the loop lasts for 17.5 + 1031 = 1048.5 cycles on average
+	call delay_1031
 _delay::
-.delay::			; 6 cycles for the CALL
-	PUSH	BC		; 4 cycles
-	CALL	.dly		; 12 cycles to return from .dly (6+1+5)
-	LD	B,#.CPMS/20-2	; 2 cycles
-				; =========
-				; 24 cycles
-.ldlp:
-	PUSH	BC		; 4 cycles
-	POP	BC		; 3 cycles
-	PUSH	BC		; 4 cycles
-	POP	BC		; 3 cycles
-	NOP			; 1 cycles
-5$:	DEC	B		; 1 cycles
-	JP	NZ,.ldlp	; 3 cycles (if TRUE: 4 cycles)
-	NOP			; 1 cycles
-				; =========
-				; 20 cycles
-	;; Exit in 16 cycles
-	POP	BC		; 3 cycles
-	PUSH	BC		; 4 cycles
-	POP	BC		; 3 cycles
-	NOP			; 1 cycles
-	NOP			; 1 cycles
-	RET			; 4 cycles
-				; =========
-				; 16 cycles
+.delay::
+	dec de
+	
+	bit 0, e
+	jr nz, 0$			; 2.5 cycles on average
+0$:
+	
+	ld a, e
+	or d
+	jr nz, loop
+	nop
 
-	;; Delay all but last millisecond
-.dly:
-	DEC	DE		; 2 cycles
-	LD	A,E		; 1 cycles
-	OR	D		; 1 cycles
-	RET	Z		; 2 cycles (upon return: 5 cycles)
-	LD	B,#.CPMS/20-1	; 2 cycles
-				; =========
-				; 8 cycles
-.dlp:
-	PUSH	BC		; 4 cycles
-	POP	BC		; 3 cycles
-	PUSH	BC		; 4 cycles
-	POP	BC		; 3 cycles
-	NOP			; 1 cycles
-	DEC	B		; 1 cycles
-	JP	NZ,.dlp		; 3 cycles (if TRUE: 4 cycles)
-	NOP			; 1 cycles
-				; =========
-				; 20 cycles
-	;; Exit in 15 cycles
-	PUSH	BC		; 4 cycles
-	POP	BC		; 3 cycles
-	NOP			; 1 cycles
-	NOP			; 1 cycles
-	JR	.dly		; 3 cycles
-				; =========
-				; 12 cycles
+delay_1031:
+	; waits for 2 + 147 * 7 = 1031 cycles
+	ld a, #147
+1$:
+	nop
+	dec a
+	ret z
+	jr 1$
