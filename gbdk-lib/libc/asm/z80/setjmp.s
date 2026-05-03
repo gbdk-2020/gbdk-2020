81c1a612b78916f0ac4146493893b8473ffa8b88
Phidias618 <126189685+Phidias618@users.noreply.github.com>
2026-05-03 22:40:58 +0200

    Update setjmp.s for the z80 port.
    I rewrote setjmp.s for the z80 port such that the calling convention __sdcccall(1) is used instead of __sdcccall(0).
    The new functions are also smaller and faster.
    ___setjmp is 7 bytes smaller and 58 cycles faster
    _longjmp is 3 bytes smaller and 38 cycles faster

--- gbdk-lib/libc/asm/z80/setjmp.s
+++ gbdk-lib/libc/asm/z80/setjmp.s
@@ -1,29 +1,8 @@
 ;--------------------------------------------------------------------------
 ;  setjmp.s
 ;
-;  Copyright (C) 2011-2014, Philipp Klaus Krause
+;  Copyright (C) 2026, Phidias618
 ;
-;  This library is free software; you can redistribute it and/or modify it
-;  under the terms of the GNU General Public License as published by the
-;  Free Software Foundation; either version 2, or (at your option) any
-;  later version.
-;
-;  This library is distributed in the hope that it will be useful,
-;  but WITHOUT ANY WARRANTY; without even the implied warranty of
-;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-;  GNU General Public License for more details.
-;
-;  You should have received a copy of the GNU General Public License 
-;  along with this library; see the file COPYING. If not, write to the
-;  Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
-;   MA 02110-1301, USA.
-;
-;  As a special exception, if you link this library with other files,
-;  some of which are compiled with SDCC, to produce an executable,
-;  this library does not by itself cause the resulting executable to
-;  be covered by the GNU General Public License. This exception does
-;  not however invalidate any other reasons why the executable file
-;   might be covered by the GNU General Public License.
 ;--------------------------------------------------------------------------
 
 	.area   _CODE
@@ -31,65 +10,73 @@
 	.globl ___setjmp
 
 ___setjmp:
-	pop	hl
-	pop	iy
-	push	af
-	push	hl
-
-	; Store return address.
-	ld	0(iy), l
-	ld	1(iy), h
+	; Store frame pointer.
+	push ix
+	pop de
+	ld (hl), e
+	inc hl
+	ld (hl), d
+	inc hl
 
 	; Store stack pointer.
-	xor	a, a
-	ld	l, a
-	ld	h, a
-	add	hl, sp
-	ld	2(iy), l
-	ld	3(iy), h
+	ex hl, de
+	ld hl, #2
+	add hl, sp
+	ex hl, de
 
-	; Store frame pointer.
-	push	ix
-	pop	hl
-	ld	4(iy), l
-	ld	5(iy), h
+	ld (hl), e
+	inc hl
+	ld (hl), d
+	inc hl
 
-	; Return 0.
-	ld	l, a
-	ld	h, a
-	ret
+	pop de		; pop return address
 
+	; Store return address.
+	ld (hl), e
+	inc hl
+	ld (hl), d
+
+	; return 0
+	ex hl, de
+	ld de, #0
+	jp (hl)
+	
 .globl _longjmp
-
 _longjmp:
-	pop	af
-	pop	iy
-	pop	de
+	pop af				; Discard return address.
 
 	; Ensure that return value is non-zero.
-	ld	a, e
-	or	a, d
-	jr	NZ, jump
-	inc	de
-jump:
+	ld a, d
+	or e
+	jr nz, 0$
+	inc d
+0$:
+	ld b, d
+	ld c, e
 
 	; Restore frame pointer.
-	ld	l, 4(iy)
-	ld	h, 5(iy)
-	push	hl
-	pop	ix
+	ld e, (hl)
+	inc hl
+	ld d, (hl)
+	inc hl
+	push de
+	pop ix
 
-	; Adjust stack pointer.
-	ld	l, 2(iy)
-	ld	h, 3(iy)
-	ld	sp, hl
-	pop	hl
+	; Restore stack pointer.
+	ld e, (hl)
+	inc hl
+	ld d, (hl)
+	inc hl
+	ex hl, de
+	ld sp, hl
+	ex hl, de
 
-	; Move return value into hl.
-	ex	de, hl
-
-	; Jump.
-	ld	c, 0(iy)
-	ld	b, 1(iy)
-	push	bc
-	ret
+	ld e, (hl)
+	inc hl
+	ld d, (hl)
+	
+	ex hl, de
+	ld d, b
+	ld e, c
+	jp (hl)
+	
