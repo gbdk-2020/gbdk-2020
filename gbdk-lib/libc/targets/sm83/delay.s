d8efa02ffa1882d3af73be666ab1ea6d8f063328
Phidias618 <126189685+Phidias618@users.noreply.github.com>
2026-05-03 12:02:16 +0200

    Update delay.s to support double speed

--- gbdk-lib/libc/targets/sm83/delay.s
+++ gbdk-lib/libc/targets/sm83/delay.s
@@ -8,29 +8,38 @@
 	;;   DE = number of milliseconds to delay (1 to 65536, 0 = 65536)
 	;; 
 	;; Register used: AF, DE
-	;; One millisecond last for 1048.576 M-cycles
-	;; This has been rounded to 1048.500 M-cycles
+	;;
+	;; One millisecond last for 1048.576 M-cycles on normal speed mode
+	;; This has been rounded to 1048.000 M-cycles
+	;; One millisecond last for 2097.152 M-cycles on double speed mode
+	;; This has been rounded to 2097.000 M-cycles
 loop:
-	; each iteration of the loop lasts for 17.5 + 1031 = 1048.5 cycles on average
-	call delay_1031
+	; on normal speed mode each iteration lasts for 6 + 1028 + 3 + 1 + 3 + 2 + 2 + 3 = 1048 cycles
+	; on double speed mode each iteration lasts for 6 + 3 + 1 + 6 + 1046 + 2 + 1 + 1 + 3 = 2097 cycles
+	call delay_1028
 _delay::
 .delay::
+
+	ldh a, (.KEY1)
+	add a
+	call c, delay_1046
+
 	dec de
-	
-	bit 0, e
-	jr nz, 0$			; 2.5 cycles on average
-0$:
 	
 	ld a, e
 	or d
 	jr nz, loop
 	nop
 
-delay_1031:
+delay_1028:
 	; waits for 2 + 147 * 7 = 1031 cycles
-	ld a, #147
-1$:
-	nop
+	ld a, #171
+L0:
 	dec a
 	ret z
-	jr 1$
+L1:
+	jr L0
+delay_1046:
+	; waits for 2 + 3 + 3 + 6 * 173 = 1046 cycles
+	ld a, #173
+	jr L1
