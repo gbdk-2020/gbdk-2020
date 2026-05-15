d40b2bfe29dfcb27ed871bbe1d6a3a52ce842dd5
Phidias618 <eliottmartin3@gmail.com>
2026-05-15 19:52:20 +0200

    removal of display_off from crt0.s

--- gbdk-lib/libc/targets/sm83/duck/crt0.s
+++ gbdk-lib/libc/targets/sm83/duck/crt0.s
@@ -439,23 +439,3 @@ gsinit::
         JR      NZ,1$
 4$:
         RET
-
-.display_off::
-_display_off::
-        ;; Check if the screen is on
-        LDH     A,(.LCDC)
-        AND     #LCDCF_ON
-        RET     Z               ; Return if screen is off
-1$:                             ; We wait for the *NEXT* VBL
-        LDH     A,(.LY)
-        CP      #0x92           ; Smaller than or equal to 0x91?
-        JR      NC,1$           ; Loop until smaller than or equal to 0x91
-2$:
-        LDH     A,(.LY)
-        CP      #0x91           ; Bigger than 0x90?
-        JR      C,2$            ; Loop until bigger than 0x90
-
-        LDH     A,(.LCDC)
-        AND     #~LCDCF_ON
-        LDH     (.LCDC),A       ; Turn off screen
-        RET
