c187b1a04aae4749b2fc88fd8b4431e99d4b2468
Phidias618 <126189685+Phidias618@users.noreply.github.com>
2026-05-17 22:47:03 +0200

    Update cgb_compat.s

--- gbdk-lib/libc/targets/sm83/cgb_compat.s
+++ gbdk-lib/libc/targets/sm83/cgb_compat.s
@@ -9,13 +9,16 @@
         
 _cgb_compatibility::
 _set_default_palette::
-	LD	HL, #1$
-	PUSH	HL
-	LD	HL, #0x0100
-	PUSH 	HL
-	CALL	_set_sprite_palette
-	CALL	_set_bkg_palette
-	ADD	SP, #4
-	RET
+	ld bc, #1$
+	xor a
+	ld e, #1
+	push bc
+	call _set_sprite_palette	; preserve BC
+	
+	xor a
+	ld e, #1
+	push bc
+	call _set_bkg_palette
+	ret
 1$:
 	.DW	0x7FFF, 0x56B5, 0x294A, 0x0000
