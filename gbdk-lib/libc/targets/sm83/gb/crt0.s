78d52c7c950226d54b76f25604c8b666d00a6c30
Toxa <untoxa@mail.ru>
2026-06-09 18:32:39 +0300

    GB/AP/MegaDuck: put display_off function out of the crt0

--- gbdk-lib/libc/targets/sm83/gb/crt0.s
+++ gbdk-lib/libc/targets/sm83/gb/crt0.s
@@ -507,23 +507,3 @@ gsinit::
         JR      NZ, 1$
 4$:
         RET
-
-.display_off::
-_display_off::
-        ;; Check if the screen is on
-        LDH     A, (rLCDC)
-        AND     #LCDCF_ON
-        RET     Z                    ; Return if screen is off
-1$:                                  ; We wait for the *NEXT* VBL
-        LDH     A, (rLY)
-        CP      #0x92                ; Smaller than or equal to 0x91?
-        JR      NC,1$                ; Loop until smaller than or equal to 0x91
-2$:
-        LDH     A, (rLY)
-        CP      #0x91                ; Bigger than 0x90?
-        JR      C, 2$                ; Loop until bigger than 0x90
-
-        LDH     A, (rLCDC)
-        AND     #~LCDCF_ON
-        LDH     (rLCDC),A            ; Turn off screen
-        RET
