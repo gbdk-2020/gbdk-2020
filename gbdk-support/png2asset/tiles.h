f0e9bf07b1674ffc6eb69e87bca76df1600ffb88
Michel Iwaniec <michel_iwaniec@yahoo.com>
2026-07-01 01:05:07 +0100

    png2asset: Fix NES tile conversion bug with 8x16 tile data

--- gbdk-support/png2asset/tiles.h
+++ gbdk-support/png2asset/tiles.h
@@ -45,10 +45,12 @@ struct Tile
         }        
         else if(pack_mode == NES) {
             for(int j = 0; j < tile_h; ++j) {
+				int Y = j / 8;
+				int y = j % 8;
                 for(int i = 0; i < 8; ++i) {
                     unsigned char col = data[8 * j + i];
-                    ret[j] |= BIT(col, 0) << (7 - i);
-                    ret[j + 8] |= BIT(col, 1) << (7 - i);
+                    ret[16 * Y + y] |= BIT(col, 0) << (7 - i);
+                    ret[16 * Y + y + 8] |= BIT(col, 1) << (7 - i);
                 }
             }
         }
